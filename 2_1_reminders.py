3abca8759b992803c1a3c38d222c847f91ae054b
denkochin <denkochinsp@gmail.com>
2021-08-18 14:41:02 +0300

    Added 2.1.5 Solution

--- 2_1_reminders.py
+++ 2_1_reminders.py
@@ -104,3 +104,34 @@ def string_price(string):
 
 # print(string_price(input()))
 
+def count_words(string):
+    '''
+    2.1.5 Количество слов
+        Дана строка, состоящая из слов, разделенных пробелами. Напишите программу, которая подсчитывает количество слов в этой строке.
+        Формат входных данных
+        На вход программе подается строка.
+        Формат выходных данных
+        Программа должна вывести количество слов в строке.
+        Примечание 1. Кроме слов в тексте могут присутствовать только пробелы (один или несколько).
+        Примечание 2. Решите задачу в одну строчку кода.
+        
+        Тестовые данные
+        Sample Input 1:
+        Hello world
+        Sample Output 1:
+        2
+        
+        Sample Input 2:
+        Timur forever young
+        Sample Output 2:
+        3
+        
+        Sample Input 3:
+        The future belongs to those who believe in beauty of their dreams
+        Sample Output 3:
+        12
+    '''
+    
+    return len(string.split())
+
+# print(count_words(input()))
